7d83d316fb8ef60811a50fcd952612c8c810cfba
GianBaeza <gianb04@gmail.com>
2024-08-06 17:10:28 -0300

    levantamos el eestado para poder crear el search desde el padre

--- src/assets/Components/Stock/Stock.jsx
+++ src/assets/Components/Stock/Stock.jsx
@@ -6,32 +6,25 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import "./stock.css";
-import { useState } from "react";
+
 import ImportExportIcon from "@mui/icons-material/ImportExport";
 import BorderColorIcon from "@mui/icons-material/BorderColor";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-const Inventario = [
-  { nombre: "Dog Food", stock: 100, codigo: 2001, lista: 50.0, venta: 45.0 },
-  { nombre: "Cat Toy", stock: 150, codigo: 2002, lista: 10.0, venta: 8.0 },
-  { nombre: "Bird Cage", stock: 50, codigo: 2003, lista: 12000, venta: 110.0 },
-  { nombre: "Fish Tank", stock: 30, codigo: 2004, lista: 200.0, venta: 180.0 },
-  { nombre: "Rabbit Food", stock: 80, codigo: 2005, lista: 25.0, venta: 20.0 },
-];
 
+//Estilos del  inventario
 const estiloHead = { fontSize: 20 };
-export default function Stock() {
-  const [inventario, setInventario] = useState(Inventario);
+const esiloinfo = { fontSize: 15 };
 
-  const handleDelete = (name) => {
-    const itemDelete = inventario.filter((item) => item.nombre !== name);
-
-    setInventario(itemDelete);
-  };
-
+export default function Stock({ inventario, handleDelete }) {
   return (
     <TableContainer
       component={Paper}
-      sx={{ width: "80%", margin: "auto", height: "auto" }}
+      sx={{
+        width: "80%",
+        margin: "auto",
+        height: "auto",
+        borderRadius: "20px",
+      }}
     >
       <Table className="table-container">
         <TableHead>
@@ -47,55 +40,65 @@ export default function Stock() {
                 }}
               />
             </TableCell>
-            <TableCell align="center" sx={estiloHead}>
+            <TableCell align="right" sx={estiloHead}>
               Stock{" "}
               <ImportExportIcon
                 className="ButtonOrden"
                 sx={{ width: 20, height: 20, margin: "auto" }}
               />
             </TableCell>
-            <TableCell align="center" sx={estiloHead}>
+            <TableCell align="right" sx={estiloHead}>
               Codigo
             </TableCell>
-            <TableCell align="center" sx={estiloHead}>
+            <TableCell align="right" sx={estiloHead}>
               $ Lista
             </TableCell>
-            <TableCell align="center" sx={estiloHead}>
+            <TableCell align="right" sx={estiloHead}>
               $ Venta
             </TableCell>
-            <TableCell align="center" sx={estiloHead}>
+            <TableCell align="right" sx={estiloHead}>
               Accion
             </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {inventario.map((inv) => (
-            <TableRow key={inv.codigo}>
-              <TableCell component="th" scope="row">
-                {inv.nombre}
-              </TableCell>
-              <TableCell align="center">{inv.stock}</TableCell>
-              <TableCell align="center">{inv.codigo}</TableCell>
-              <TableCell align="center">
-                $ {new Intl.NumberFormat("es-AR").format(inv.lista)}
-              </TableCell>
-              <TableCell align="center">
-                {" "}
-                $ {new Intl.NumberFormat("es-AR").format(inv.venta)}
-              </TableCell>
-              <TableCell align="center">
-                <button className="acciones">
-                  <BorderColorIcon />
-                </button>
-                <button
-                  className="acciones"
-                  onClick={() => handleDelete(inv.nombre)}
-                >
-                  <DeleteForeverIcon />
-                </button>
-              </TableCell>
-            </TableRow>
-          ))}
+          {inventario.length > 0 ? (
+            inventario.map((inv) => (
+              <TableRow key={inv.codigo}>
+                <TableCell component="th" scope="row" sx={esiloinfo}>
+                  {inv.nombre}
+                </TableCell>
+                <TableCell align="right" sx={esiloinfo}>
+                  {inv.stock}
+                </TableCell>
+                <TableCell align="right" sx={esiloinfo}>
+                  {inv.codigo}
+                </TableCell>
+                <TableCell align="right" sx={esiloinfo}>
+                  $ {new Intl.NumberFormat("es-AR").format(inv.lista)}
+                </TableCell>
+                <TableCell align="right" sx={esiloinfo}>
+                  {" "}
+                  $ {new Intl.NumberFormat("es-AR").format(inv.venta)}
+                </TableCell>
+                <TableCell align="right">
+                  <div className="button-container">
+                    <button className="acciones">
+                      <BorderColorIcon />
+                    </button>
+                    <button
+                      className="acciones"
+                      onClick={() => handleDelete(inv.nombre)}
+                    >
+                      <DeleteForeverIcon />
+                    </button>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <span>Inventario vacio</span>
+          )}
         </TableBody>
       </Table>
     </TableContainer>
--- src/assets/Components/StockInventario/StockInventario.jsx
+++ src/assets/Components/StockInventario/StockInventario.jsx
@@ -1,8 +1,30 @@
 import Stock from "../Stock/Stock";
 import "../StockInventario/stockInventario.css";
 import SearchIcon from "@mui/icons-material/Search";
+import { useState } from "react";
 
+const Inventario = [
+  { nombre: "Dog Food", stock: 100, codigo: 2001, lista: 50.0, venta: 45.0 },
+  { nombre: "Cat Toy", stock: 150, codigo: 2002, lista: 10.0, venta: 8.0 },
+  { nombre: "Bird Cage", stock: 50, codigo: 2003, lista: 12000, venta: 110.0 },
+  {
+    nombre: "Fish Tank",
+    stock: 30,
+    codigo: 2004,
+    lista: 200.0,
+    venta: 18330.0,
+  },
+  { nombre: "Rabbit Food", stock: 80, codigo: 2005, lista: 25.0, venta: 20.0 },
+];
 export default function StockInventario() {
+  const [inventario, setInventario] = useState(Inventario);
+
+  const handleDelete = (name) => {
+    const itemDelete = inventario.filter((item) => item.nombre !== name);
+
+    setInventario(itemDelete);
+  };
+
   return (
     <div className="container-Stock">
       <header className="main-Stock">
@@ -12,19 +34,19 @@ export default function StockInventario() {
           <button>Nuevo Articulo</button>
           <form action="/search" method="get">
             <div className="input-container">
+              <SearchIcon className="search-icon" />
               <input
                 type="search"
                 name="search"
                 id="buscarItem"
                 placeholder="Buscar Articulo.."
               />
-              <SearchIcon className="search-icon" />
             </div>
           </form>
         </section>
       </header>
       <section className="container-table">
-        <Stock />
+        <Stock handleDelete={handleDelete} inventario={inventario} />
       </section>
     </div>
   );
