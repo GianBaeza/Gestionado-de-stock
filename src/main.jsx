1c36d0903e2989cd784e439d662174b69b65cd4d
GianBaeza <gianb04@gmail.com>
2024-08-28 21:55:35 -0300

    aad analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import ReactDOM from 'react-dom/client'
 import React from 'react'
 import App from './App'
 import './index.css'
-
+import { Analytics } from "@vercel/analytics/react"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <Analytics>
+            <App />
+        </Analytics>
+
     </React.StrictMode>,
 )
