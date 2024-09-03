41ee531b6c406ce0ca52c5ec84dffe5b47f5f7e1
Gian Franco Baeza <gianb04@gmail.com>
2024-09-03 10:50:52 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "../src/app.css";
 import InventarioProvider from "./Components/Context/StockContext";
 import StockInventario from "./Components/StockInventario/StockInventario";
-import ThemeContextProvider from "./Components/Context/ThemeContext"; // Asegúrate de que este es el archivo correcto
+import ThemeContextProvider from "./Components/Context/ThemeContext"; 
 
 function App() {
 
